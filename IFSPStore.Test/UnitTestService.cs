d0f73c24dc45af0ea7eb1f5cdf34db7ba660b699
Murilo <murilo.varges@gmail.com>
2024-11-12 12:01:41 -0300

    Implementado serviço.

---
+++ IFSPStore.Test/UnitTestService.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using IFSPStore.Domain.Base;
+using IFSPStore.Domain.Entities;
+using IFSPStore.Repository.Context;
+using IFSPStore.Repository.Repository;
+using IFSPStore.Service.Services;
+using IFSPStore.Service.Validators;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+namespace IFSPStore.Test
+{
+    [TestClass]
+    public class UnitTestService
+    {
+        private ServiceCollection? services;
+
+        public ServiceProvider ConfigureServices()
+        {
+            services = new ServiceCollection();
+            services.AddDbContext<MySqlContext>(options =>
+            {
+                var server = "localhost";
+                var port = "3306";
+                var database = "IFSPStore";
+                var username = "root";
+                var password = "ifsp2024";
+                var strCon = $"Server={server};Port={port};Database={database};" +
+                            $"Uid={username};Pwd={password}";
+                options.UseMySql(strCon, ServerVersion.AutoDetect(strCon), opt =>
+                {
+                    opt.CommandTimeout(180);
+                    opt.EnableRetryOnFailure(5);
+                });
+            });
+
+            services.AddScoped<IBaseRepository<Usuario>, BaseRepository<Usuario>>();
+            services.AddScoped<IBaseService<Usuario>, BaseService<Usuario>>();
+            services.AddSingleton(new MapperConfiguration(config => 
+                                { 
+                                    config.CreateMap<Usuario, Usuario>(); 
+                                }).CreateMapper());
+            return services.BuildServiceProvider();
+        }
+
+        [TestMethod]
+        public void TestInsertUsuario()
+        {
+            var sp = ConfigureServices();
+            var usuarioService = sp.GetService<IBaseService<Usuario>>();
+            var usuario = new Usuario
+            {
+                Nome = "Murilo",
+                Login = "mvs",
+                Email = "murilo@gmail.com",
+                Senha = "123Rt@#jdjfhfh!",
+                DataCadastro = DateTime.Now
+            };
+
+            var result = usuarioService.Add<Usuario, Usuario, UsuarioValidator>(usuario);
+            Console.Write(JsonSerializer.Serialize(result));
+        }
+
+        [TestMethod]
+        public void TestSelectUsuario()
+        {
+            var sp = ConfigureServices();
+            var usuarioService = sp.GetService<IBaseService<Usuario>>();
+           
+            var result = usuarioService.Get<Usuario>();
+            Console.Write(JsonSerializer.Serialize(result));
+        }
+    }
+}
